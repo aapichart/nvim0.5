5cc131403504ed84e49d71d44f9f73e5069dffab
aapichart <chart.asa@gmail.com>
2021-10-23 23:08:51 +0700

    Remove recursive update out

--- .config/.config/i3/startKitty.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-kitty @ set-font-size 8 
-kitty @ launch --title NetMonitor --type=tab --tab-title "NetMonitor" --keep-focus gping 10.135.70.65
-kitty @ launch --match title:NetMonitor --type=window --keep-focus wavemon 
--- .config/.config/i3/startKittyMonServ.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-kitty @ launch  --title Client --type=tab --tab-title "Client" --keep-focus zsh 
-kitty @ launch  --title Server --type=tab --tab-title "Server" --keep-focus zsh 
-
